44af8e45d5d322021472f838100c7a32f6de2410
StrixLuca <110634650+StrixLuca@users.noreply.github.com>
2025-08-13 20:33:52 +0200

    drops

--- client/drops/functions.lua
+++ client/drops/functions.lua
@@ -1,39 +1,35 @@
 Drops = {}
-
 Drops.ResetPlayerState = function() 
-    -- we run loadskin to drop lootbag if possible
     if LocalPlayer.state.holdingDrop then
-        ExecuteCommand('loadskin') 
+        ExecuteCommand('loadskin')
     end
     LocalPlayer.state.holdingDrop = nil
     LocalPlayer.state.dropBagObject = nil
     LocalPlayer.state.heldDrop = nil
 end
-
 Drops.GetDrops = function()
     RSGCore.Functions.TriggerCallback('rsg-inventory:server:GetCurrentDrops', function(drops)
         if not drops then return end
-        for k, v in pairs(drops) do
-            local bag = NetworkGetEntityFromNetworkId(v.entityId)
+
+        for dropId, dropData in pairs(drops) do
+            local bag = NetworkGetEntityFromNetworkId(dropData.entityId)
+
             if DoesEntityExist(bag) then
-                exports['rsg-target']:AddTargetEntity(bag, {
-                    options = {
-                        {
-                            icon = 'fas fa-backpack',
-                            label = Lang:t('menu.o_bag'),
-                            action = function()
-                                TriggerServerEvent('rsg-inventory:server:openDrop', k)
-                                LocalPlayer.state.currentDrop = k
-                            end,
-                        },
-                    },
-                    distance = 2.5,
+                exports.ox_target:addEntity(bag, {
+                    {
+                        icon = 'fas fa-backpack',
+                        label = locale('o_bag'),
+                        onSelect = function()
+                            TriggerServerEvent('rsg-inventory:server:openDrop', dropId)
+                            LocalPlayer.state.currentDrop = dropId
+                        end,
+                        distance = 2.5
+                    }
                 })
             end
         end
     end)
 end
-
 
 
 
