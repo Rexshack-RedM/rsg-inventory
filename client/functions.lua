cfa990a90b781c056739a7bf22fb77ff83aea92d
StrixLuca <110634650+StrixLuca@users.noreply.github.com>
2025-08-14 14:45:48 +0200

    why not local

--- client/functions.lua
+++ client/functions.lua
@@ -1,14 +1,7 @@
-Inventory = {}
 local config = require 'shared.config'
+Inventory = {}
 
-function Inventory.CanPlayerUseInventory()
-    local playerData = RSGCore.Functions.GetPlayerData()
-    if not playerData or not playerData.metadata then return false end
-    local meta = playerData.metadata
-    return not meta.isdead and not meta.ishandcuffed
-end
-
-function Inventory.NotifyHotbarSpamProtection()
+local function notifyHotbarSpamProtection()
     if not config.HotbarSpamProtectionNotify then return end
     lib.notify({
         title       = locale('error.error'),
@@ -18,12 +11,21 @@ function Inventory.NotifyHotbarSpamProtection()
     })
 end
 
+function Inventory.CanPlayerUseInventory()
+    local playerData = RSGCore.Functions.GetPlayerData()
+    if not playerData or not playerData.metadata then return false end
+    local meta = playerData.metadata
+    return not meta.isdead and not meta.ishandcuffed
+end
+
 function Inventory.UseHotbarItem(slot)
     local currentTime = GetGameTimer()
     local lastUsed = LocalPlayer.state.hotbarLastUsed or 0
+
     if currentTime - lastUsed < config.HotbarSpamProtectionTimeout then
-        return Inventory.NotifyHotbarSpamProtection()
+        return notifyHotbarSpamProtection()
     end
+
     LocalPlayer.state.hotbarLastUsed = currentTime
 
     local playerData = RSGCore.Functions.GetPlayerData()
@@ -41,36 +43,3 @@ function Inventory.UseHotbarItem(slot)
 
     TriggerServerEvent('rsg-inventory:server:useItem', itemData)
 end
-
-
-
-
--- Optioneel: wapens attachments formatteren
---[[ 
-function Inventory.FormatWeaponAttachments(itemData)
-    if not (itemData.info and itemData.info.attachments and #itemData.info.attachments > 0) then
-        return {}
-    end
-
-    local attachments = {}
-    local weaponName = itemData.name
-    local WeaponAttachments = exports['rsg-weapons']:getConfigWeaponAttachments()
-    if not WeaponAttachments then return {} end
-
-    for attachmentType, weapons in pairs(WeaponAttachments) do
-        local componentHash = weapons[weaponName]
-        if componentHash then
-            for _, attachment in pairs(itemData.info.attachments) do
-                if attachment.component == componentHash then
-                    local label = RSGCore.Shared.Items[attachmentType] and RSGCore.Shared.Items[attachmentType].label or 'Unknown'
-                    table.insert(attachments, {
-                        attachment = attachmentType,
-                        label = label
-                    })
-                end
-            end
-        end
-    end
-    return attachments
-end
-]]
